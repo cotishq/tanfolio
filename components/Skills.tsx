7f6936eae8e3f0a53d21d4023485021a7dfc037a
cotishq <tanishqp101204@gmail.com>
2026-04-30 01:24:56 +0530

    updated skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,26 +1,26 @@
-import { FaReact, FaNodeJs, FaGithub, FaGitAlt, FaJava } from "react-icons/fa";
+import { FaNodeJs, FaGitAlt, FaJava, FaAws, FaLinux } from "react-icons/fa";
 import { 
-  SiNextdotjs, 
-  SiTailwindcss, 
-  SiPrisma, 
   SiPostgresql, 
   SiJavascript, 
   SiTypescript, 
   SiVercel, 
   SiPostman, 
   SiMongodb, 
-  SiShadcnui, 
   SiExpress, 
-  SiHtml5, 
-  SiCss3, 
   SiRust, 
-  SiDrizzle, 
-  SiFigma, 
-  SiWeb3Dotjs, 
-  SiClerk, 
-  SiGo
+  SiGo,
+  SiDocker,
+  SiKubernetes,
+  SiTerraform,
+  SiHelm,
+  SiMinio,
+  SiNginx,
+  SiGooglecloud,
+  SiRedis,
+  SiSqlite
 } from "react-icons/si";
-import { VscVscode } from "react-icons/vsc";
+import { VscAzure, VscVscode } from "react-icons/vsc";
+import { BiLogoGoLang } from "react-icons/bi";
 import { SkillBadge } from "./Skillbadge";
 
 
@@ -41,55 +41,70 @@ const Skills = () => {
             &lt; Languages /&gt;
           </p>
           <div className="flex flex-wrap gap-2">
-            <SkillBadge name="HTML" icon={<SiHtml5 />} />
-            <SkillBadge name="CSS" icon={<SiCss3 />} />
-            <SkillBadge name="JavaScript" icon={<SiJavascript />} />
-            <SkillBadge name="TypeScript" icon={<SiTypescript />} />
             <SkillBadge name="Go" icon={<SiGo />} />
             <SkillBadge name="Rust" icon={<SiRust />} />
+            <SkillBadge name="TypeScript" icon={<SiTypescript />} />
+            <SkillBadge name="JavaScript" icon={<SiJavascript />} />
             <SkillBadge name="Java" icon={<FaJava />} />
           </div>
         </div>
 
         <div className="space-y-3">
           <p className="font-mono text-xs text-muted-foreground tracking-wider uppercase">
-            &lt; Frameworks & Libs /&gt;
+            &lt; Backend /&gt;
           </p>
           <div className="flex flex-wrap gap-2">
-            <SkillBadge name="React" icon={<FaReact />} />
-            <SkillBadge name="Next.js" icon={<SiNextdotjs />} />
-            <SkillBadge name="TailwindCSS" icon={<SiTailwindcss />} />
-            <SkillBadge name="Express.js" icon={<SiExpress />} />
-            <SkillBadge name="Web3.js" icon={<SiWeb3Dotjs />} />
-            <SkillBadge name="Shadcn/ui" icon={<SiShadcnui />} />
+            <SkillBadge name="Echo" icon={<BiLogoGoLang />} />
+            <SkillBadge name="Gin" icon={<BiLogoGoLang />} />
+            <SkillBadge name="Node.js" icon={<FaNodeJs />} />
+            <SkillBadge name="Express" icon={<SiExpress />} />
+            <SkillBadge name="gRPC" />
+            <SkillBadge name="REST APIs" />
+            <SkillBadge name="Websockets" />
           </div>
         </div>
 
         <div className="space-y-3">
           <p className="font-mono text-xs text-muted-foreground tracking-wider uppercase">
-            &lt; Backend & Database /&gt;
+            &lt; Databases /&gt;
           </p>
           <div className="flex flex-wrap gap-2">
-            <SkillBadge name="Node.js" icon={<FaNodeJs />} />
-            <SkillBadge name="Clerk" icon={<SiClerk />} />
             <SkillBadge name="PostgreSQL" icon={<SiPostgresql />} />
             <SkillBadge name="MongoDB" icon={<SiMongodb />} />
-            <SkillBadge name="Prisma" icon={<SiPrisma />} />
-            <SkillBadge name="Drizzle" icon={<SiDrizzle />} />
+            <SkillBadge name="Redis" icon={<SiRedis />} />
+            <SkillBadge name="SQLite" icon={<SiSqlite />} />
           </div>
         </div>
 
         <div className="space-y-3">
           <p className="font-mono text-xs text-muted-foreground tracking-wider uppercase">
-            &lt; Developer Tools /&gt;
+            &lt; Infrastructure /&gt;
+          </p>
+          <div className="flex flex-wrap gap-2">
+            <SkillBadge name="Docker" icon={<SiDocker />} />
+            <SkillBadge name="Kubernetes" icon={<SiKubernetes />} />
+            <SkillBadge name="Terraform" icon={<SiTerraform />} />
+            <SkillBadge name="Helm" icon={<SiHelm />} />
+            <SkillBadge name="AWS" icon={<FaAws />} />
+            <SkillBadge name="GCP" icon={<SiGooglecloud />} />
+            <SkillBadge name="Azure" icon={<VscAzure />} />
+            <SkillBadge name="NGINX" icon={<SiNginx />} />
+            <SkillBadge name="MINIIO" icon={<SiMinio />} />
+
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <p className="font-mono text-xs text-muted-foreground tracking-wider uppercase">
+            &lt; Tools /&gt;
           </p>
           <div className="flex flex-wrap gap-2">
             <SkillBadge name="Git" icon={<FaGitAlt />} />
-            <SkillBadge name="GitHub" icon={<FaGithub />} />
+            <SkillBadge name="Linux" icon={<FaLinux />} />
+            <SkillBadge name="Postman" icon={<SiPostman />} />
             <SkillBadge name="VS Code" icon={<VscVscode />} />
             <SkillBadge name="Vercel" icon={<SiVercel />} />
-            <SkillBadge name="Postman" icon={<SiPostman />} />
-            <SkillBadge name="Figma" icon={<SiFigma />} />
+
           </div>
         </div>
       </div>
